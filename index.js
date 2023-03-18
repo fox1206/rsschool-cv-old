cff483578d22d6437bac3255423fbe6c778a283c
Mari <wourtex99@gmail.com>
2023-03-18 19:56:13 +0300

    feat: add change for education and experience

---
+++ index.js
@@ -0,0 +1,16 @@
+const tabs = document.querySelector('.about-me__tabs');
+const aboutSection = document.querySelector('.about-me__section');
+
+tabs.addEventListener('click', (event) => {
+  // console.log('click');
+  // console.log(event.target);
+  if(event.target.classList.contains('tab-item') && !event.target.classList.contains('active')){
+    tabs.querySelector('.active').classList.remove('active');
+    event.target.classList.add('active');
+
+    const section = event.target.getAttribute('data-target');
+    // console.log(section);
+    aboutSection.querySelector('.tab__content.active').classList.remove('active');
+    aboutSection.querySelector(section).classList.add('active');
+  }
+});
