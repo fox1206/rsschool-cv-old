4333086a5ee85a1e5bfe82b21f7c66e53ea508ba
Mari <wourtex99@gmail.com>
2023-03-19 20:45:20 +0300

    feat: add function update content

--- index.js
+++ index.js
@@ -17,11 +17,40 @@ tabs.addEventListener('click', (event) => {
 });
 
 
-/* ================== Открытие портфолио =================== */
+/* ================== Открытие / закрытие портфолио =================== */
+const btnClose = document.querySelector('.project-window__close');
+const main = document.querySelector('.main');
+const picture = document.querySelector('.project-windows__picture img');
+const headerPicture = document.querySelector('.project-window__head h3');
+const description = document.querySelector('.project-windows__info .description p');
+const info = document.querySelector('.project-windows__info .back-info li')
+
+const projectInformation = (item) => {
+  // console.log(item);
+  picture.src = item.querySelector('.project__item-box img').src;
+  
+  headerPicture.innerHTML = item.querySelector('.project__item-title').innerHTML;
+  
+  description.innerHTML = item.querySelector('.project__item-details .description p').innerHTML;
+
+  info.array.forEach(element => {
+    console.log(element);
+  });
+  // info.innerHTML = item.querySelector('.project__item-details .back-info li').innerHTML;
+}
+
 document.addEventListener('click', (event) => {
   if(event.target.classList.contains('view-project')){
     // console.log('click');
     document.querySelector('.project-window').classList.toggle('open');
-    
+    main.classList.toggle('out');
+    projectInformation(event.target.parentElement);
   }
 });
+
+btnClose.addEventListener('click', () => {
+  document.querySelector('.project-window').classList.toggle('open');
+  main.classList.toggle('out');
+});
+
+
